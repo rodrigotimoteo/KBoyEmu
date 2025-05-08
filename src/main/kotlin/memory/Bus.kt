3fef8c251f4de361bf3437dd6bdccd542b3035ba
rodrigotimoteo <rodrigo.timoteo2603@gmail.com>
2025-05-08 15:43:10 +0100

    Started building the Bus and created the first Bus constants that will later be used to execute specific actions from the CPU to the memory itself (and get specific values as well)

--- src/main/kotlin/memory/Bus.kt
+++ src/main/kotlin/memory/Bus.kt
@@ -4,9 +4,10 @@ package memory
  * @author rodrigotimoteo
  **/
 class Bus(
-
+    private val isCGB: Boolean,
+    private val rom: MemoryModule
 ) {
-
+    private val memoryManager: MemoryManager = MemoryManager(this, rom)
 
     fun isCGB(): Boolean {
         return false
---
+++ src/main/kotlin/memory/BusConstants.kt
@@ -0,0 +1,45 @@
+package memory
+
+/**
+ * @author rodrigotimoteo
+ **/
+enum class BusConstants(
+    val id: Int
+) {
+
+    /**
+     * CPU Getter Codes
+     */
+    GET_FLAGS(0),
+    GET_REGISTER(1),
+    GET_AF(2),
+    GET_BC(3),
+    GET_DE(4),
+    GET_HL(5),
+    GET_PC(6),
+    GET_SP(7),
+    GET_HALTED(8),
+    GET_STOPPED(9),
+    GET_MC(10),
+    GET_HALT_MC(11),
+
+    /**
+     * CPU Execution Codes
+     */
+    TICK_TIMERS(0),
+    SET_REGISTER(1),
+    INCR_PC(2),
+    SET_PC(3),
+    INCR_SP(4),
+    SET_SP(5),
+    SET_AF(6),
+    SET_BC(7),
+    SET_DE(8),
+    SET_HL(9),
+    DISABLE_INT(10),
+    ENABLE_INT(11),
+    REQUEST_INT(12),
+    HALT(13),
+    UNHALT(14),
+    STOP(15)
+}
